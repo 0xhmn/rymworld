f900ae5e0ef388e0d7380dcfe04662ff9fa2fa55
Hugh Yar <hooman.yar@gmail.com>
2018-10-14 15:07:18 -0700

    Init commit

---
+++ Assets/Controller/WorldController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    World world;
+
+	// Use this for initialization
+	void Start () {
+        world = new World();
+        Debug.Log("World is created!");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Model/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Tile {
+
+    public enum TileType { Empty, Floor };
+
+    // Default Tile.
+    TileType type = TileType.Empty;
+
+    // Each Tile can have an object.
+    LoosObject loosObject;
+    InstalledObject installedObject;
+
+    World world;
+    int x;
+    int y;
+
+    public Tile(World world, int x, int y)
+    {
+        this.world = world;
+        this.x = x;
+        this.y = y;
+    }
+}
